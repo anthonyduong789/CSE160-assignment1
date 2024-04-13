29ab1e58a7eea8d217187f77ebd816eb71334cb9
Anthony Duong <anthonyduong789@gmail.com>
2024-04-12 18:20:15 -0700

    made changes with shapes

--- ColoredPoints.js
+++ ColoredPoints.js
@@ -2,9 +2,10 @@
 // Vertex shader program
 var VSHADER_SOURCE =
   "attribute vec4 a_Position;\n" +
+  "uniform float u_Size;\n" +
   "void main() {\n" +
   "  gl_Position = a_Position;\n" +
-  "  gl_PointSize = 10.0;\n" +
+  "  gl_PointSize = u_Size;\n" +
   "}\n";
 
 // Fragment shader program
@@ -19,13 +20,14 @@ let canvas;
 let gl;
 let a_Position;
 let u_FragColor;
-
+let u_Size;
 function setupWebGL() {
   // Retrieve <canvas> element
   canvas = document.getElementById("webgl");
 
   // Get the rendering context for WebGL
-  gl = getWebGLContext(canvas);
+  // gl = getWebGLContext(canvas);
+  gl = canvas.getContext("webgl", { preserveDrawingBuffer: true });
   if (!gl) {
     console.log("Failed to get the rendering context for WebGL");
     return;
@@ -51,62 +53,124 @@ function connectVariablesToGLSL() {
     console.log("Failed to get the storage location of u_FragColor");
     return;
   }
+  // Get the storage location of u_Size
+  u_Size = gl.getUniformLocation(gl.program, "u_Size");
 }
 
+// Global variables related to the color and size of the points
+let g_selectedColor = [1.0, 1.0, 1.0, 1.0];
+let selectedSize = 5;
+let g_selectedShape;
+function addActionsForHtmlUI() {
+  // this function get the color from the sliders to be used in the drawing
+  document.querySelector("#redSlide").addEventListener("mouseup", function () {
+    g_selectedColor[0] = this.value / 100;
+  });
+  document
+    .querySelector("#greenSlide")
+    .addEventListener("mouseup", function () {
+      g_selectedColor[1] = this.value / 100;
+    });
+  document.querySelector("#blueSlide").addEventListener("mouseup", function () {
+    g_selectedColor[2] = this.value / 100;
+  });
+  // size of the slider
+  document.getElementById("shapeSize").addEventListener("mouseup", function () {
+    selectedSize = this.value;
+  });
+  document.getElementById("clearButton").addEventListener("click", function () {
+    g_shapeLists = [];
+    renderAllShapes();
+  });
+  document
+    .getElementById("turnOnSquare")
+    .addEventListener("click", function () {
+      g_selectedShape = "square";
+    });
+  document
+    .getElementById("turnOnTriangle")
+    .addEventListener("click", function () {
+      g_selectedShape = "triangle";
+    });
+  document
+    .getElementById("turnOnCircle")
+    .addEventListener("click", function () {
+      g_selectedShape = "circle";
+    });
+}
 function main() {
   // Set up canvas and gl variables
   setupWebGL();
   // sets up the shadow programs and also the
   connectVariablesToGLSL();
-  // Register function (event handler) to be called on a mouse press
-  canvas.onmousedown = function (ev) {
-    click(ev, gl, canvas, a_Position, u_FragColor);
+  // register function (event handler) to be called on a mouse press
+  canvas.onmousedown = click;
+  // checks to see if the button was being pressed while the mouse was moving
+  canvas.onmousemove = function (ev) {
+    if (ev.buttons == 1) click(ev);
   };
-
   // Specify the color for clearing <canvas>
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  addActionsForHtmlUI();
 
-  // Clear <canvas>
+  // // Clear <canvas>
   gl.clear(gl.COLOR_BUFFER_BIT);
 }
+var g_shapeLists = [];
+// var g_points = []; // The array for the position of a mouse press
+// var g_colors = []; // The array to store the color of a point
+// var g_sizes = []; // The array to store the size of a point
+function click(ev) {
+  [x, y] = convertCoordinatesEventToGL(ev);
+  let point;
+  if (g_selectedShape == "square") {
+    point = new Square();
+  } else if (g_selectedShape == "triangle") {
+    point = new Triangle();
+  } else if (g_selectedShape == "circle") {
+    point = new Circle();
+  }
+  point.position = [x, y];
+  point.color = g_selectedColor.slice();
+  point.size = selectedSize;
+  g_shapeLists.push(point);
+  // console.log(g_shapeLists);
+  // Store the coordinates to g_points array
+  // g_points.push([x, y]);
 
-var g_points = []; // The array for the position of a mouse press
-var g_colors = []; // The array to store the color of a point
-function click(ev, gl, canvas, a_Position, u_FragColor) {
+  // g_colors.push(g_selectedColor.slice());
+  // // console.log(g_selectedColor);
+  // g_sizes.push(selectedSize);
+  // Store the coordinates to g_points array
+  // if (x >= 0.0 && y >= 0.0) {
+  // First quadrant
+  // g_colors.push([1.0, 0.0, 0.0, 1.0]); // Red
+  // } else if (x < 0.0 && y < 0.0) {
+  // // Third quadrant
+  // g_colors.push([0.0, 1.0, 0.0, 1.0]); // Green
+  // } else {
+  // // Others
+  // g_colors.push([1.0, 1.0, 1.0, 1.0]); // White
+  // }
+  renderAllShapes();
+}
+
+function convertCoordinatesEventToGL(ev) {
   var x = ev.clientX; // x coordinate of a mouse pointer
   var y = ev.clientY; // y coordinate of a mouse pointer
   var rect = ev.target.getBoundingClientRect();
 
   x = (x - rect.left - canvas.width / 2) / (canvas.width / 2);
   y = (canvas.height / 2 - (y - rect.top)) / (canvas.height / 2);
-
-  // Store the coordinates to g_points array
-  g_points.push([x, y]);
-  // Store the coordinates to g_points array
-  if (x >= 0.0 && y >= 0.0) {
-    // First quadrant
-    g_colors.push([1.0, 0.0, 0.0, 1.0]); // Red
-  } else if (x < 0.0 && y < 0.0) {
-    // Third quadrant
-    g_colors.push([0.0, 1.0, 0.0, 1.0]); // Green
-  } else {
-    // Others
-    g_colors.push([1.0, 1.0, 1.0, 1.0]); // White
-  }
-
+  return [x, y];
+}
+// this function draws all the shaepes
+function renderAllShapes() {
   // Clear <canvas>
   gl.clear(gl.COLOR_BUFFER_BIT);
 
-  var len = g_points.length;
+  var len = g_shapeLists.length;
   for (var i = 0; i < len; i++) {
-    var xy = g_points[i];
-    var rgba = g_colors[i];
-
-    // Pass the position of a point to a_Position variable
-    gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
-    // Pass the color of a point to u_FragColor variable
-    gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
-    // Draw
-    gl.drawArrays(gl.POINTS, 0, 1);
+    g_shapeLists[i].render();
   }
 }
